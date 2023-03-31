10148a4cc3d40436c4c8a95524b470e885e3119a
Mingjie Jiang <13333578+itsmingjie@users.noreply.github.com>
2023-03-30 22:27:11 -0500

    fixes

--- apps/web/src/ui/topbar/topbar.tsx
+++ apps/web/src/ui/topbar/topbar.tsx
@@ -1,5 +1,5 @@
 export const TopBar = (): JSX.Element => {
   return (
-    <div className="min-h-12 sticky top-0 z-10 flex flex-col justify-center border-b border-hint/50 bg-dark px-3 py-2 opacity-5 blur-xl saturate-[180%] before:content-['\200b']"></div>
+    <div className="sticky top-0 z-10 flex min-h-[48px] flex-col justify-center border-b border-hint/50 bg-dark px-3 py-2 opacity-5 blur-xl saturate-[180%] before:content-['\u200b']" />
   );
 };
